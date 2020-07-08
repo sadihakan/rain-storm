b7283e986a882061b68f59228b655816f0eaf3db
Hakan <hakankosanoglu@hotmail.com>
2020-07-08 13:00:24 +0300

    first commit

---
+++ Rainstorm/View Controllers/Root View Controller/RootViewController.swift
@@ -0,0 +1,22 @@
+//
+//  RootViewController.swift
+//  Rainstorm
+//
+//  Created by Hakan Koşanoğlu on 8.07.2020.
+//  Copyright © 2020 com.kai. All rights reserved.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+    
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
